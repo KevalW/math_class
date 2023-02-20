a5686cef1bce153061230e0b23239711996ff399
Keval <keval.waghate@gmail.com>
2023-02-20 22:17:17 +0530

    just added two buttons

---
+++ app/src/main/java/com/example/math_class/question_setup.kt
@@ -0,0 +1,11 @@
+package com.example.math_class
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class question_setup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question_setup)
+    }
+}
